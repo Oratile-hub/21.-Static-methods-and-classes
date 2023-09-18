d8792124e580646b29936cb7a75e2e39da76eefe
Nomthandazo Ndweni <ndweni2658@gmail.com>
2023-09-18 11:46:48 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.Static_methods_and_classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHi salute = new SayHi();
+
+            //salute.sayHello("Oratile"); Cannot be accessed by an object since the method is static
+
+            SayHi.sayHello("Oratile");
+            
+
+            Console.WriteLine(Math.Sqrt(144));
+
+            //freeze console
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SayHi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.Static_methods_and_classes
+{
+    internal class SayHi
+    {
+
+        public static void sayHello(string name)
+        {
+            Console.WriteLine("Hello " + name);
+        
+        }
+    }
+}
